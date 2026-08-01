15f4741e14d7282010d9dfad16096ffa9c37f3a9
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-08-01 20:48:07 +0200

    refactor: streamline error handling and variable checks in NGINX entrypoint script

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -8,22 +8,28 @@ CONFIG_FILE="/etc/nginx/conf.d/default.conf"
 CERTIFICATE_FILE="/etc/nginx/ssl/inception.crt"
 PRIVATE_KEY_FILE="/etc/nginx/ssl/inception.key"
 
-: "${DOMAIN_NAME:?DOMAIN_NAME is not set}"
-
-[ -r "$TEMPLATE_FILE" ] || {
-	echo "NGINX template is not readable: $TEMPLATE_FILE" >&2
+fail()
+{
+	echo "NGINX entrypoint error: $1" >&2
 	exit 1
 }
 
-[ -r "$CERTIFICATE_FILE" ] || {
-	echo "TLS certificate is not readable: $CERTIFICATE_FILE" >&2
-	exit 1
+require_variable()
+{
+	eval "value=\${$1:-}"
+	[ -n "$value" ] || fail "$1 is not set"
 }
 
-[ -r "$PRIVATE_KEY_FILE" ] || {
-	echo "TLS private key is not readable: $PRIVATE_KEY_FILE" >&2
-	exit 1
+require_file()
+{
+	[ -r "$1" ] || fail "cannot read file: $1"
 }
+
+require_variable DOMAIN_NAME
+
+require_file "$TEMPLATE_FILE"
+require_file "$CERTIFICATE_FILE"
+require_file "$PRIVATE_KEY_FILE"
 
 envsubst '${DOMAIN_NAME}' \
 	< "$TEMPLATE_FILE" \
